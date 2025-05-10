4e081f0a3831da104650d2a7ea17f2903f0c228b
Akatsuki667 <qextzy@gmail.com>
2025-05-10 22:53:01 +0200

    implémentation stockagetableau.java, readme.md

---
+++ Java/src/stockage/StockageTableau.java
@@ -0,0 +1,75 @@
+package src.stockage;
+
+import src.modele.Tache;
+public class StockageTableau implements StockageTaches {
+    // Attributs
+    Tache[] tableauTache; // Stockage taches
+    int nbTaches; // Compteur tâches
+
+    // Constructeur
+    public StockageTableau() {
+        // Initilaisation tableau taille fixe
+        this.tableauTache = new Tache[10];
+        this.nbTaches = 0;
+    }
+
+    // Implémentation méthode addTache()
+    @Override
+    public void addTache(Tache nouvelleTache) {
+        // Vérification stockage tableau valide
+        if (this.nbTaches < this.tableauTache.length) {
+            // Stockage nouvelle tache à l'indice nbTaches
+            tableauTache[nbTaches] = nouvelleTache;
+            // Incrémentation nbTaches création nouvelle indice pour tableauTache
+            nbTaches++;
+        } else {
+            System.out.println("Stockage plein");
+        }
+    }
+
+    // Implémentation méthode allTache()
+    @Override
+    public Tache[] allTache() {
+        // Création nouveauTableau, instanciation de la classe Tache
+        Tache[] nouveauTableau = new Tache[nbTaches];
+        // Boucle qui copie tableauTache vers nouveauTableau
+        for (int i = 0; i < nbTaches; i++) {
+            nouveauTableau[i] = tableauTache[i];
+        }
+        return nouveauTableau;
+    }
+
+    // Implémentation méthode getByIndice()
+    @Override
+    public Tache getByIndice(int indice) {
+        // Vérification que l'indice est valide
+        if (indice >= 0 && indice < nbTaches) {
+            // Si valide, retourne la tâche à cet indice
+            return tableauTache[indice];
+        } else {
+            // Si invalide, affiche un message et retourne null
+            System.out.println("Indice invalide");
+            return null;
+        }
+    }
+
+    // Implémentation méthode majTache
+    @Override
+    public boolean majTache(int indice, Tache nouvelleTache) {
+        // Vérification que l'indice est valide
+        if (indice >= 0 && indice < nbTaches) {
+            // Si valide, remplace la tâche à cet indice
+            tableauTache[indice] = nouvelleTache;
+            return true;
+        } else {
+            // Si invalide, affiche un message et retourne false
+            System.out.println("Indice invalide");
+            return false;
+        }
+    }
+
+    // Implémentation méthode totalTache
+    public int totalTache() {
+        return nbTaches;
+    }
+}
--- Java/src/stockage/StockageTaches.java
+++ Java/src/stockage/StockageTaches.java
@@ -5,13 +5,13 @@ import src.modele.Tache;
 public interface StockageTaches {
     // Méthode d'interface
     // Méthode ajout d'une tâche
-    public void addTache(Tache tache);
+    public void addTache(Tache nouvelleTache);
     // Méthode récupération de toutes les tâches
     public Tache[] allTache();
     // Méthode récupération tâche avec son indice
     public Tache getByIndice(int indice);
     //Méthode MAJ une tâche
-    public boolean majTache(int indice, Tache tache);
+    public boolean majTache(int indice, Tache nouvelleTache);
     // Méthode obtenir nb de tâche
     public int totalTache();
 }
