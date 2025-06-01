cd96fe8a81977083b83174897946a067e4eadc8d
Akatsuki667 <qextzy@gmail.com>
2025-06-01 20:00:32 +0200

    Documentation Spring Initializr, API Pêche

---
+++ Spring/API Pêche/src/main/java/com/example/API/Peche/ApiPecheApplication.java
@@ -0,0 +1,13 @@
+package com.example.API.Peche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPecheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPecheApplication.class, args);
+	}
+
+}
