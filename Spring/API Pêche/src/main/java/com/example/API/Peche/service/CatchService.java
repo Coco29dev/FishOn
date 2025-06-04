67d292f45d69d1f4cbfce5993405510d7b8295b0
Akatsuki667 <qextzy@gmail.com>
2025-06-04 11:02:42 +0200

    Documentation Service, implémentation CatchService.java

--- Spring/API Pêche/src/main/java/com/example/API/Peche/service/CatchService.java
+++ Spring/API Pêche/src/main/java/com/example/API/Peche/service/CatchService.java
@@ -1,18 +1,61 @@
 package com.example.API.Peche.service;
 
+// Annotation pour l'injection automatique des dépendances Spring
 import org.springframework.beans.factory.annotation.Autowired;
+// Annotation pour marquer une classe comme service (logique métier)
 import org.springframework.stereotype.Service;
+// Interface Java pour manipuler des listes d'objets
+import java.util.List;
 
 import com.example.API.Peche.repository.CatchRepository;
+import com.example.API.Peche.repository.UserRepository;
 
-@Service
+import com.example.API.Peche.model.Catch;
+import com.example.API.Peche.model.User;
+
+@Service // Dit à Spring classe contenant logique métier
 public class CatchService {
 
-    @Autowired
+    @Autowired // Injection automatique
     private CatchRepository catchRepository;
 
-    @Autowired
-    private UserService userService;
+    @Autowired // Injection automatique
+    private UserRepository userRepository;
 
-    
+    // Méthode ajouter prise
+    public Catch addCatches(Long userId, String fishName, Double weight, String location) {
+
+        // Vérification existance user_id
+        if (!userRepository.existsById(userId)) {
+            throw new RuntimeException("Utilisateur inexistant");
+        }
+
+        // Récupération objet User complet
+        User user = userRepository.findById(userId).orElse(null);
+
+        // Création objet Catch
+        Catch newCatch = new Catch(fishName, weight, location, user);
+
+        // Sauvegarde prise
+        Catch savedCatch = catchRepository.save(newCatch);
+        return savedCatch;
+    }
+
+    // Méthode récupération prise utilisateur
+    public List<Catch> getUserCatches(Long userId) {
+        return catchRepository.findByUserId(userId);
+    }
+
+    // Méthode récupération prise par nom de poisson
+    public List<Catch> getCatchesByName(String fishName) {
+        // Vérification paramètres valide
+        if (fishName == null || fishName.trim().isEmpty())
+        // .trim() = Supprime les espaces début et fin
+        // .isEmpty() = Vérification string est vide 
+        {
+            throw new RuntimeException("Nom de poisson inconnue");
+        } else {
+            return catchRepository.findByFishName(fishName);
+        }
+    }
 }
