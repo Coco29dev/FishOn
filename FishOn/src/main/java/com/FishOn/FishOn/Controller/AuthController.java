de53d041ab6e72746105d4c8d69c76789f035981
Coco29dev <10059@holbertonstudents.com>
2025-06-14 14:37:49 +0200

    Implémentation AuthController

--- FishOn/src/main/java/com/FishOn/FishOn/Controller/AuthController.java
+++ FishOn/src/main/java/com/FishOn/FishOn/Controller/AuthController.java
@@ -1,19 +1,13 @@
 package com.FishOn.FishOn.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.FishOn.FishOn.Exception.FishOnException.EmailAlreadyExists;
-import com.FishOn.FishOn.Exception.FishOnException.UserAlreadyExists;
+import org.springframework.web.bind.annotation.*;
+import com.FishOn.FishOn.DTO.Auth.*;
+import com.FishOn.FishOn.Exception.FishOnException.*;
 import com.FishOn.FishOn.Model.UserModel;
 import com.FishOn.FishOn.Service.AuthService;
-import com.FishOn.FishOn.Service.UserService;
 
 import jakarta.validation.Valid;
-
 
 @RestController
 @RequestMapping("/api/auth")
@@ -22,13 +16,56 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @Autowired
-    private UserService userService;
 
     @PostMapping("/register")
-    public UserModel register(@Valid @RequestBody UserModel user) throws EmailAlreadyExists, UserAlreadyExists // @RequestBody = Conversion JSON -> User object
-    {
+    public RegisterResponseDTO register (@Valid @RequestBody RegisterRequestDTO registerRequest) throws  EmailAlreadyExists, UserAlreadyExists {
+
+        // Conversion de DTO à Model
+        UserModel user = new UserModel(
+                registerRequest.getUserName(),
+                registerRequest.getEmail(),
+                registerRequest.getFirstName(),
+                registerRequest.getLastName(),
+                registerRequest.getAge(),
+                registerRequest.getPassword(),
+                registerRequest.getProfilePicture()
+        );
+
+        // Appel de AuthService pour récupérer/utiliser la méthode register
         UserModel newUser = authService.register(user);
-        return newUser;
+
+        // Conversion de Model à DTO
+        RegisterResponseDTO response = new RegisterResponseDTO(
+                newUser.getId(),
+                newUser.getUserName(),
+                newUser.getEmail(),
+                newUser.getFirstName(),
+                newUser.getLastName(),
+                newUser.getAge(),
+                newUser.getProfilePicture(),
+                newUser.getCreatedAt()
+        );
+
+        return response;
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDTO login (@Valid @RequestBody LoginRequestDTO loginRequest) throws UserNotFoundByEmail, InvalidPassword {
+
+        // Appel service pour récupérer/utiliser la méthode login avec comme paramètre email + password
+        UserModel user = authService.login(loginRequest.getEmail(), loginRequest.getPassword());
+
+        // Conversion de Model à DTO
+        LoginResponseDTO response = new LoginResponseDTO(
+                user.getId(),
+                user.getUserName(),
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getAge(),
+                user.getProfilePicture()
+        );
+
+        return response;
     }
 }
