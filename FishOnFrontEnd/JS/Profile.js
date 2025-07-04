f4e5225d8b77bd52bfdb9d69e7f32cf1771d2a87
Akatsuki667 <qextzy@gmail.com>
2025-07-05 00:21:35 +0200

    Implémentation profile.js terminé

--- FishOnFrontEnd/JS/Profile.js
+++ FishOnFrontEnd/JS/Profile.js
@@ -46,6 +46,117 @@ class Profile {
         Utils.message(result.error, 'error');
     }
   }
-}
 
+  // ========== DONNÉES UTILISATEURS ==========
+  static async loadUserProfile() {
+      const result = await APIService.getProfile();
+
+      if (!result.success) {
+        throw new Error(result.error);
+      }
+
+      const profileData = result.data;
+
+      const profilePicture = document.querySelector('.profile-picture');
+      Utils.setupAvatar(profilePicture, profileData.profilePicture, profileData.userName);
+      document.getElementById('userName').textContent = profileData.userName;
+      document.getElementById('age').textContent = `${profileData.age} ans`;
+  }
+
+  // ========== GESTION PUBLICATIONS ==========
+  static async loadUserPosts() {
+    const result = await APIService.getCurrentUserPosts();
+    if (!result.success) {
+      throw new Error(result.error);
+    }
+
+    const posts = result.data;
+
+    document.getElementById('userPostCount').textContent = posts.length;
+    if (posts.length === 0) {
+      return;
+    }
+
+    // Tri par date (plus récent en premier)
+    const sortedPosts = Utils.sortPostsByDate(posts);
+
+    // Affichage de la dernière publication
+    this.displayLastPost(sortedPosts[0]);
+
+    // Affichage des souvenirs
+    if (sortedPosts.length > 1) {
+      this.displayMemories(sortedPosts.slice(1));
+    }
+  }
+
+  static displayLastPost(post) {
+    // Récupération template
+    const template = document.getElementById('lastTemplate');
+    const lastPost = template.content.cloneNode(true);
+
+    // Récupération image
+    const fishPicture = lastPost.querySelector('.fish-picture');
+    Utils.setupPostImage(fishPicture, post.photoUrl);
+
+    // Récupération champs obligatoires
+    lastPost.querySelector('.post-title').textContent = post.title;
+    lastPost.querySelector('.post-description').textContent = post.description;
+    lastPost.querySelector('.post-fishname').textContent = `Poisson: ${post.fishName}`;
+
+    // Récupération champs optionnels
+    if (post.weight) {
+      lastPost.querySelector('.post-weight').textContent = `Poids: ${post.weight} kg`;
+    }
+    if (post.length) {
+      lastPost.querySelector('.post-length').textContent = `Longueur: ${post.length} cm`;
+    }
+    if (post.location) {
+      lastPost.querySelector('.post-location').textContent = `Lieu: ${post.location}`;
+    }
+
+    // Date de création
+    lastPost.querySelector('.post-date').textContent = Utils.formatDate(post.createdAt);
+
+    // Injection dans le DOM
+    const container = document.getElementById('lastPost');
+    container.appendChild(lastPost);
+  }
+
+  static displayMemories(posts) {
+    const container = document.getElementById('postsMemories');
+    const template = document.getElementById('memoriesTemplate');
+  
+    // Boucle sur toutes les publications "souvenirs"
+    posts.forEach(post => {
+      // Récupération template
+      const memory = template.content.cloneNode(true);
+  
+      // Récupération image
+      const fishPicture = memory.querySelector('.fish-picture');
+      Utils.setupPostImage(fishPicture, post.photoUrl);
+  
+      // Récupération champs obligatoires
+      memory.querySelector('.post-title').textContent = post.title;
+      memory.querySelector('.post-description').textContent = post.description;
+      memory.querySelector('.post-fishname').textContent = `Poisson: ${post.fishName}`;
+  
+      // Récupération champs optionnels
+      if (post.weight) {
+        memory.querySelector('.post-weight').textContent = `Poids: ${post.weight} kg`;
+      }
+      if (post.length) {
+        memory.querySelector('.post-length').textContent = `Longueur: ${post.length} cm`;
+      }
+      if (post.location) {
+        memory.querySelector('.post-location').textContent = `Lieu: ${post.location}`;
+      }
+  
+      // Date de création
+      memory.querySelector('.post-date').textContent = Utils.formatDate(post.createdAt);
+  
+      // Injection dans le DOM
+      container.appendChild(memory);
+    });
+  }
+}
 document.addEventListener('DOMContentLoaded', Profile.init);
